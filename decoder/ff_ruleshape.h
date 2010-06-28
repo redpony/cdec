f96bf4df7e4a34b42373723cbe38e6c7425e3239
redpony <redpony@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-06-28 20:40:28 +0000

    rule shape features
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@46 ec762483-ff6d-05da-a07a-a48fb63a330f

---
+++ decoder/ff_ruleshape.h
@@ -0,0 +1,31 @@
+#ifndef _FF_RULESHAPE_H_
+#define _FF_RULESHAPE_H_
+
+#include <vector>
+#include "ff.h"
+
+class RuleShapeFeatures : public FeatureFunction {
+ public:
+  RuleShapeFeatures(const std::string& param);
+ protected:
+  virtual void TraversalFeaturesImpl(const SentenceMetadata& smeta,
+                                     const Hypergraph::Edge& edge,
+                                     const std::vector<const void*>& ant_contexts,
+                                     SparseVector<double>* features,
+                                     SparseVector<double>* estimated_features,
+                                     void* context) const;
+ private:
+  struct Node {
+    int fid_;
+    Node() : fid_(-1) {}
+    std::map<bool, Node> next_;
+  };
+  Node fidtree_;
+  static const Node* Advance(const Node* cur, bool val) {
+    std::map<bool, Node>::const_iterator it = cur->next_.find(val);
+    if (it == cur->next_.end()) return NULL;
+    return &it->second;
+  }
+};
+
+#endif
