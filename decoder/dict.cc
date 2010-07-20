0b09bcf26266c4af528c39a27e6ddb885c197c57
philblunsom <philblunsom@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-07-20 23:42:11 +0000

    Fixed major bug with spaces in regex.
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@348 ec762483-ff6d-05da-a07a-a48fb63a330f

--- decoder/dict.cc
+++ decoder/dict.cc
@@ -6,6 +6,6 @@
 #include <boost/algorithm/string/regex.hpp>
 
 void Dict::AsVector(const WordID& id, std::vector<std::string>* results) const {
-  boost::algorithm::split_regex(*results, Convert(id), boost::regex(" \\|\\|\\| "));
+  boost::algorithm::split_regex(*results, Convert(id), boost::regex("\\s\\|\\|\\|\\s"));
 }
 
