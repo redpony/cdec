86edeae1e7f84408057a0dbd7902661b2cae7c07
graehl <graehl@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-07-21 15:51:24 +0000

    vest generate options valgrind fix
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@356 ec762483-ff6d-05da-a07a-a48fb63a330f

--- decoder/filelib.h
+++ decoder/filelib.h
@@ -59,9 +59,9 @@ class ReadFile : public BaseFile<std::istream> {
         abort();
       }
       char const* file=filename_.c_str(); // just in case the gzstream keeps using the filename for longer than the constructor, e.g. inflateReset2.  warning in valgrind that I'm hoping will disappear - it makes no sense.
-      ps_.reset(EndsWith(filename, ".gz") ?
+      ps_=PS(EndsWith(filename, ".gz") ?
                 static_cast<std::istream*>(new igzstream(file)) :
-                static_cast<std::istream*>(new std::ifstream(file)));
+             static_cast<std::istream*>(new std::ifstream(file)));
       if (!*ps_) {
         std::cerr << "Failed to open " << filename << std::endl;
         abort();
@@ -81,7 +81,7 @@ class WriteFile : public BaseFile<std::ostream> {
       ps_=PS(&std::cout,file_null_deleter());
     } else {
       char const* file=filename_.c_str(); // just in case the gzstream keeps using the filename for longer than the constructor, e.g. inflateReset2.  warning in valgrind that I'm hoping will disappear - it makes no sense.
-      ps_.reset(EndsWith(filename, ".gz") ?
+      ps_=PS(EndsWith(filename, ".gz") ?
                 static_cast<std::ostream*>(new ogzstream(file)) :
                 static_cast<std::ostream*>(new std::ofstream(file)));
       if (!*ps_) {
