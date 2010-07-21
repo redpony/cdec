c946ad175601eda5a8cb3e6cd0e7c973d3656012
graehl <graehl@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-07-21 20:52:35 +0000

    tdict TD:: ss se unk and reserved(i)
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@362 ec762483-ff6d-05da-a07a-a48fb63a330f

--- decoder/ff_fsa.h
+++ decoder/ff_fsa.h
@@ -6,6 +6,7 @@
 #include "ff.h"
 #include "sparse_vector.h"
 #include "value_array.h"
+#include "tdict.h"
 
 typedef ValueArray<uint8_t> Bytes;
 
@@ -32,8 +33,18 @@ struct FsaFeatureFunction {
 
 // regular bottom up scorer from Fsa feature
 template <class Impl>
-struct FeatureFunctionFromFsa : public FeatureFunction,Impl {
-  FeatureFunctionFromFsa(
+struct FeatureFunctionFromFsa : public FeatureFunction {
+  Impl& d() { return static_cast<Impl&>(*this); }
+  Impl const& d() { return static_cast<Impl const&>(*this); }
+
+  FeatureFunctionFromFsa() {  }
+  Init() {
+    name=d().name;
+    SetStateSize(sizeof(WordID)*2*MarkovOrder
+  } // can't do this in constructor because we come before d() in order
+
+  virtual Features Features() const { return d().Features(); }
+
 };
 
 
