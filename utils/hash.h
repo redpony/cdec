12f187c555300fd7dc2873b604bd70dc11583d80
graehl <graehl@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-08-11 03:10:07 +0000

    get_default mapped_type
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@517 ec762483-ff6d-05da-a07a-a48fb63a330f

--- utils/hash.h
+++ utils/hash.h
@@ -53,7 +53,7 @@ struct murmur_hash_array
 
 // adds default val to table if key wasn't found, returns ref to val
 template <class H,class K>
-typename H::data_type & get_default(H &ht,K const& k,typename H::data_type const& v) {
+typename H::mapped_type & get_default(H &ht,K const& k,typename H::mapped_type const& v) {
   return const_cast<typename H::data_type &>(ht.insert(typename H::value_type(k,v)).first->second);
 }
 
