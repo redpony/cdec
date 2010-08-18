24fde5b80aa3b1b735d6ac6f18e08d0012ebebf8
graehl <graehl@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-08-18 01:58:24 +0000

    home
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@588 ec762483-ff6d-05da-a07a-a48fb63a330f

--- decoder/hg.h
+++ decoder/hg.h
@@ -72,7 +72,7 @@ public:
   // TODO get rid of edge_prob_? (can be computed on the fly as the dot
   // product of the weight vector and the feature values)
   struct Edge {
-//    int poplimit; //TODO: cube pruning per edge limit?  per node didn't work well at all.
+//    int poplimit; //TODO: cube pruning per edge limit?  per node didn't work well at all.  also, inside cost + outside(node) is the same information i'd use to set a per-edge limit anyway - and nonmonotonicity in cube pruning may mean it's good to favor edge (in same node) w/ relatively worse score
     Edge() : i_(-1), j_(-1), prev_i_(-1), prev_j_(-1) {}
     Edge(int id,Edge const& copy_pod_from) : id_(id) { copy_pod(copy_pod_from); } // call copy_features yourself later.
     Edge(int id,Edge const& copy_from,TailNodeVector const& tail) // fully inits - probably more expensive when push_back(Edge(...)) than setting after
