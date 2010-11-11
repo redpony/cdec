2936b9e333ec0280b0ed3e115af23f13ab8b87a7
redpony <redpony@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-11-11 20:31:56 +0000

    klm stub
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@711 ec762483-ff6d-05da-a07a-a48fb63a330f

---
+++ decoder/ff_klm.h
@@ -0,0 +1,32 @@
+#ifndef _KLM_FF_H_
+#define _KLM_FF_H_
+
+#include <vector>
+#include <string>
+
+#include "ff.h"
+
+struct KLanguageModelImpl;
+
+class KLanguageModel : public FeatureFunction {
+ public:
+  // param = "filename.lm [-o n]"
+  KLanguageModel(const std::string& param);
+  ~KLanguageModel();
+  virtual void FinalTraversalFeatures(const void* context,
+                                      SparseVector<double>* features) const;
+  static std::string usage(bool param,bool verbose);
+  Features features() const;
+ protected:
+  virtual void TraversalFeaturesImpl(const SentenceMetadata& smeta,
+                                     const Hypergraph::Edge& edge,
+                                     const std::vector<const void*>& ant_contexts,
+                                     SparseVector<double>* features,
+                                     SparseVector<double>* estimated_features,
+                                     void* out_context) const;
+ private:
+  int fid_; // conceptually const; mutable only to simplify constructor
+  KLanguageModelImpl* pimpl_;
+};
+
+#endif
