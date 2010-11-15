24d2bfb3ace1ae15ffee661602a7d03ef2599e48
redpony <redpony@ec762483-ff6d-05da-a07a-a48fb63a330f>
2010-11-15 17:07:04 +0000

    model 1 options
    git-svn-id: https://ws10smt.googlecode.com/svn/trunk@723 ec762483-ff6d-05da-a07a-a48fb63a330f

--- training/mr_em_adapted_reduce.cc
+++ training/mr_em_adapted_reduce.cc
@@ -6,35 +6,14 @@
 #include <boost/program_options.hpp>
 #include <boost/program_options/variables_map.hpp>
 
-#include "config.h"
-#ifdef HAVE_BOOST_DIGAMMA
-#include <boost/math/special_functions/digamma.hpp>
-using boost::math::digamma;
-#endif
-
 #include "filelib.h"
 #include "fdict.h"
 #include "weights.h"
 #include "sparse_vector.h"
+#include "em_utils.h"
 
 using namespace std;
 namespace po = boost::program_options;
-
-#ifndef HAVE_BOOST_DIGAMMA
-#warning Using Mark Johnsons digamma()
-double digamma(double x) {
-  double result = 0, xx, xx2, xx4;
-  assert(x > 0);
-  for ( ; x < 7; ++x)
-    result -= 1/x;
-  x -= 1.0/2.0;
-  xx = 1.0/x;
-  xx2 = xx*xx;
-  xx4 = xx2*xx2;
-  result += log(x)+(1./24.)*xx2-(7.0/960.0)*xx4+(31.0/8064.0)*xx4*xx2-(127.0/30720.0)*xx4*xx4;
-  return result;
-}
-#endif
 
 void InitCommandLine(int argc, char** argv, po::variables_map* conf) {
   po::options_description opts("Configuration options");
