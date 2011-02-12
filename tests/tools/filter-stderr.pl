16b144a0f440a56a56b27c811aace7f6fecb6f09
Chris Dyer <cdyer@cs.cmu.edu>
2011-02-12 16:13:34 -0500

    fix tests

--- tests/tools/filter-stderr.pl
+++ tests/tools/filter-stderr.pl
@@ -4,10 +4,10 @@ use strict;
 my $REAL = '(\+|-)?[1-9][0-9]*(\.[0-9]*)?|(\+|-)?[1-9]\.[0-9]*e(\+|-)?[0-9]+';
 
 while(<>) {
-if (/-LM forest\s+\(nodes\/edges\): (\d+)\/(\d+)/) { print "-lm_nodes $1\n-lm_edges $2\n"; }
-if (/-LM forest\s+\(paths\): (.+)$/) { print "-lm_paths $1\n"; }
+if (/Init.*forest\s+\(nodes\/edges\): (\d+)\/(\d+)/) { print "-lm_nodes $1\n-lm_edges $2\n"; }
+if (/Init.*forest\s+\(paths\): (.+)$/) { print "-lm_paths $1\n"; }
 # -LM Viterbi: -12.7893
-if (/-LM\s+Viterbi:\s+($REAL)/) {
+if (/Init.*\s+Viterbi:\s+($REAL)/) {
   print "-lm_viterbi $1\n";
 } elsif (/-LM\s+Viterbi:\s+(.+)$/) {
   # -LM Viterbi: australia is have diplomatic relations with north korea one of the few countries .
@@ -18,11 +18,11 @@ if (/-LM\s+Viterbi:\s+($REAL)/) {
 if (/Constr\. forest\s+\(nodes\/edges\): (\d+)\/(\d+)/) { print "constr_nodes $1\nconstr_edges $2\n"; }
 if (/Constr\. forest\s+\(paths\): (.+)$/) { print "constr_paths $1\n"; }
 
-if (/\+LM forest\s+\(nodes\/edges\): (\d+)\/(\d+)/) { print "+lm_nodes $1\n+lm_edges $2\n"; }
-if (/\+LM forest\s+\(paths\): (.+)$/) { print "+lm_paths $1\n"; }
-if (/\+LM\s+Viterbi:\s+($REAL)/) {
+if (/Pass1.*forest\s+\(nodes\/edges\): (\d+)\/(\d+)/) { print "+lm_nodes $1\n+lm_edges $2\n"; }
+if (/Pass1.*forest\s+\(paths\): (.+)$/) { print "+lm_paths $1\n"; }
+if (/Pass1.*Viterbi:\s+($REAL)/) {
   print "+lm_viterbi $1\n";
-} elsif (/\+LM\s+Viterbi:\s+(.+)$/) {
+} elsif (/Pass1.*Viterbi:\s+(.+)$/) {
   # -LM Viterbi: australia is have diplomatic relations with north korea one of the few countries .
   print "+lm_trans $1\n";
 }
