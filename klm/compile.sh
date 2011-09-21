f111672dd611f78656fceb3df3729a290453ef56
Kenneth Heafield <kenlm@kheafield.com>
2011-09-21 18:23:50 -0400

    Updated kenlm.  Includes left state support but not the cdec-side use of it.  Updated binary format.

--- klm/compile.sh
+++ klm/compile.sh
@@ -3,10 +3,12 @@
 #If your code uses ICU, edit util/string_piece.hh and uncomment #define USE_ICU
 #I use zlib by default.  If you don't want to depend on zlib, remove #define USE_ZLIB from util/file_piece.hh
 
+#don't need to use if compiling with moses Makefiles already
+
 set -e
 
-for i in util/{bit_packing,ersatz_progress,exception,file_piece,murmur_hash,scoped,mmap} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,trie,virtual_interface,vocab}; do
-  g++ -I. -O3 $CXXFLAGS -c $i.cc -o $i.o
+for i in util/{bit_packing,ersatz_progress,exception,file_piece,murmur_hash,file,mmap} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,trie,trie_sort,virtual_interface,vocab}; do
+  g++ -I. -O3 -DNDEBUG $CXXFLAGS -c $i.cc -o $i.o
 done
-g++ -I. -O3 $CXXFLAGS lm/build_binary.cc {lm,util}/*.o -lz -o build_binary
-g++ -I. -O3 $CXXFLAGS lm/ngram_query.cc {lm,util}/*.o -lz -o query
+g++ -I. -O3 -DNDEBUG $CXXFLAGS lm/build_binary.cc {lm,util}/*.o -lz -o build_binary
+g++ -I. -O3 -DNDEBUG $CXXFLAGS lm/ngram_query.cc {lm,util}/*.o -lz -o query
