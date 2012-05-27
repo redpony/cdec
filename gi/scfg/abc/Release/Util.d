ab38dc57a6a64aa7ef60a845a4176e18e1ac7f27
Chris Dyer <cdyer@cab.ark.cs.cmu.edu>
2012-05-27 00:43:48 -0400

    remove dead code

--- gi/scfg/abc/Release/Util.d
+++
@@ -1,8 +0,0 @@
-Util.d Util.o: ../../utils/Util.cc ../../utils/Util.h \
- ../../utils/UtfConverter.h ../../utils/ConvertUTF.h
-
-../../utils/Util.h:
-
-../../utils/UtfConverter.h:
-
-../../utils/ConvertUTF.h:
