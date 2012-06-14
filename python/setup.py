74b6f83f4e7aed662e104df00bb3d02c937d09e6
Victor Chahuneau <vchahune@cs.cmu.edu>
2012-06-14 09:27:53 -0400

    Detect boost location before building the python module

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,22 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
+
+INC = ['..', 'src/', '../decoder', '../utils']
+LIB = ['../decoder', '../utils', '../mteval', '../klm/lm', '../klm/util']
+
+BOOST_ROOT = os.getenv('BOOST_ROOT')
+if BOOST_ROOT:
+    INC.append(os.path.join(BOOST_ROOT, 'include/'))
+    LIB.append(os.path.join(BOOST_ROOT, 'lib/'))
 
 ext_modules = [
     Extension(name='_cdec',
         sources=['src/_cdec.pyx'],
         language='C++', 
-        include_dirs=['..', 'src/', '../decoder', '../utils'],
-        library_dirs=['../decoder', '../utils', '../mteval', '../klm/lm', '../klm/util'],
+        include_dirs=INC,
+        library_dirs=LIB,
         libraries=['boost_program_options-mt', 'z',
                    'cdec', 'utils', 'mteval', 'klm', 'klm_util'])
 ]
