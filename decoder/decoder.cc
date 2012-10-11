b7005385f267596436dd07b1a9e798023ef1c30a
Kenneth Heafield <github@kheafield.com>
2012-10-11 06:02:26 -0400

    Skip rest of decoder when using incremental

--- decoder/decoder.cc
+++ decoder/decoder.cc
@@ -834,8 +834,11 @@ bool DecoderImpl::Decode(const string& input, DecoderObserver* o) {
   if (conf.count("show_target_graph"))
     HypergraphIO::WriteTarget(conf["show_target_graph"].as<string>(), sent_id, forest);
 
-  if (conf.count("lazy_search"))
+  if (conf.count("lazy_search")) {
     PassToLazy(conf["lazy_search"].as<string>().c_str(), CurrentWeightVector(), pop_limit, forest);
+    o->NotifyDecodingComplete(smeta);
+    return true;
+  }
 
   for (int pass = 0; pass < rescoring_passes.size(); ++pass) {
     const RescoringPass& rp = rescoring_passes[pass];
