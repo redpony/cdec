45c932f9cb26f0144f3f5ce26b64d60e44f9b854
Patrick Simianer <p@simianer.de>
2013-04-25 10:29:57 +0200

    added Gesa's parse match feature function

---
+++ decoder/ff_parse_match.h
@@ -0,0 +1,25 @@
+#ifndef _FF_PARSE_MATCH_H_
+#define _FF_PARSE_MATCH_H_
+
+#include "ff.h"
+#include "hg.h"
+
+struct ParseMatchFeaturesImpl;
+
+class ParseMatchFeatures : public FeatureFunction {
+ public:
+  ParseMatchFeatures(const std::string& param);
+  ~ParseMatchFeatures();
+ protected:
+  virtual void TraversalFeaturesImpl(const SentenceMetadata& smeta,
+                                     const Hypergraph::Edge& edge,
+                                     const std::vector<const void*>& ant_contexts,
+                                     SparseVector<double>* features,
+                                     SparseVector<double>* estimated_features,
+                                     void* context) const;
+  virtual void PrepareForInput(const SentenceMetadata& smeta);
+ private:
+  ParseMatchFeaturesImpl* impl;
+};
+
+#endif
