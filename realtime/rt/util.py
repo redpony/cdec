b67c9b33ca9e91fc0200a6687fc76f73c0a022f3
Michael Denkowski <mdenkows@cs.cmu.edu>
2013-09-15 12:01:44 -0700

    Fixed pathes for mkconfig

--- realtime/rt/util.py
+++ realtime/rt/util.py
@@ -14,28 +14,31 @@ SA_INI_FILES = set((
     ))
 
 def cdec_ini_for_config(config):
-    cdec_ini_handle(config, os.path.basename, hpyplm_rm_ref)
-
-def cdec_ini_for_realtime(config, path, ref_fifo):
-    cdec_ini_handle(config, lambda x: os.path.join(path, x), lambda x: hpyplm_add_ref(x, ref_fifo))
-
-def cdec_ini_handle(config, path_fn, hpyplm_fn):
     # This is a list of (k, v), not a ConfigObj or dict
     for i in range(len(config)):
         if config[i][0] == 'feature_function':
             if config[i][1].startswith('KLanguageModel'):
                 f = config[i][1].split()
-                f[-1] = path_fn(f[-1])
+                f[-1] = 'mono.klm'
+                config[i][1] = ' '.join(f)
+            elif config[i][1].startswith('External'):
+                config[i][1] = 'External libcdec_ff_hpyplm.so corpus.hpyplm'
+
+def cdec_ini_for_realtime(config, path, ref_fifo):
+    # This is a list of (k, v), not a ConfigObj or dict
+    for i in range(len(config)):
+        if config[i][0] == 'feature_function':
+            if config[i][1].startswith('KLanguageModel'):
+                f = config[i][1].split()
+                f[-1] = os.path.join(path, f[-1])
                 config[i][1] = ' '.join(f)
             elif config[i][1].startswith('External'):
                 f = config[i][1].split()
-                if f[1].endswith('libcdec_ff_hpyplm.so'):
-                    # Modify paths
-                    for j in range(1, len(f)):
-                        if not f[j].startswith('-'):
-                            f[j] = path_fn(f[j])
-                    # Modify hpyplm args
-                    hpyplm_fn(f)
+                f[1] =  os.path.join(path, f[1])
+                f[2] =  os.path.join(path, f[2])
+                f.append('-r')
+                f.append(ref)
+                f.append('-t')
                 config[i][1] = ' '.join(f)
 
 def consume_stream(stream):
@@ -43,18 +46,6 @@ def consume_stream(stream):
         for _ in s:
             pass
     threading.Thread(target=consume, args=(stream,)).start()
-
-def hpyplm_add_ref(f, ref):
-    f.append('-r')
-    f.append(ref)
-    f.append('-t')
-
-def hpyplm_rm_ref(f):
-    for i in range(1, len(f)):
-        if f[i] == '-r':
-            f.pop(i)
-            f.pop(i)
-            return
 
 def popen_io(cmd):
     p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
