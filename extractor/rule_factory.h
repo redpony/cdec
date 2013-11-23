9cc2e002a064a2e14444669178126d1e96be8230
Paul Baltescu <pauldb89@gmail.com>
2013-11-23 18:39:39 +0000

    Fix broken extractor test.

--- extractor/rule_factory.h
+++ extractor/rule_factory.h
@@ -72,7 +72,10 @@ class HieroCachingRuleFactory {
 
   // Constructs SCFG rules for a given sentence.
   // (See class description for more details.)
-  virtual Grammar GetGrammar(const vector<int>& word_ids, const unordered_set<int>& blacklisted_sentence_ids, const shared_ptr<DataArray> source_data_array);
+  virtual Grammar GetGrammar(
+      const vector<int>& word_ids,
+      const unordered_set<int>& blacklisted_sentence_ids,
+      const shared_ptr<DataArray> source_data_array);
 
  protected:
   HieroCachingRuleFactory();
