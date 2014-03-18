55beb71dbbfe8421a8e8be9d2d7868a5d87d77d5
Chris Dyer <redpony@gmail.com>
2014-03-18 01:55:19 -0400

    star bool

--- utils/star.h
+++ utils/star.h
@@ -1,12 +1,18 @@
 #ifndef _STAR_H_
 #define _STAR_H_
 
+// star(x) computes the infinite sum x^0 + x^1 + x^2 + ...
+
 template <typename T>
 T star(const T& x) {
   if (!x) return T();
   if (x > T(1)) return std::numeric_limits<T>::infinity();
   if (x < -T(1)) return -std::numeric_limits<T>::infinity();
   return T(1) / (T(1) - x);
+}
+
+bool star(bool x) {
+  return x;
 }
 
 #endif
