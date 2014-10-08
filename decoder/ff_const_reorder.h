8c46c1eaf8ccc70488da9203ac93cb7265cd734f
Wu, Ke <wuke@cs.umd.edu>
2014-10-08 06:31:14 -0400

    Apply clang-format on ff_const_reorder.*

--- decoder/ff_const_reorder.h
+++ decoder/ff_const_reorder.h
@@ -16,26 +16,26 @@ struct ConstReorderFeatureImpl;
 class ConstReorderFeature : public FeatureFunction {
  public:
   // param = "filename n"
-	ConstReorderFeature(const std::string& param);
-    ~ConstReorderFeature();
-    static std::string usage(bool param,bool verbose);
+  ConstReorderFeature(const std::string& param);
+  ~ConstReorderFeature();
+  static std::string usage(bool param, bool verbose);
+
  protected:
   virtual void PrepareForInput(const SentenceMetadata& smeta);
 
-  virtual void TraversalFeaturesImpl(const SentenceMetadata& smeta,
-                                     const HG::Edge& edge,
-                                     const std::vector<const void*>& ant_contexts,
-                                     SparseVector<double>* features,
-                                     SparseVector<double>* estimated_features,
-                                     void* out_context) const;
+  virtual void TraversalFeaturesImpl(
+      const SentenceMetadata& smeta, const HG::Edge& edge,
+      const std::vector<const void*>& ant_contexts,
+      SparseVector<double>* features, SparseVector<double>* estimated_features,
+      void* out_context) const;
+
  private:
   ConstReorderFeatureImpl* pimpl_;
 };
 
-
 struct ConstReorderFeatureFactory : public FactoryBase<FeatureFunction> {
   FP Create(std::string param) const;
-  std::string usage(bool params,bool verbose) const;
+  std::string usage(bool params, bool verbose) const;
 };
 
 #endif /* FF_CONST_REORDER_H_ */
