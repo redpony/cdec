638e28db2b2146e8920ec105792d2b03a0de5c3e
Chris Dyer <redpony@gmail.com>
2015-04-09 00:18:27 -0700

    fix kenlm's use of compression libraries

--- config.h.cmake
+++ config.h.cmake
@@ -1,8 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
- 
+
 #cmakedefine METEOR_JAR "@METEOR_JAR@"
 #cmakedefine HAVE_ZLIB @HAVE_ZLIB@
 #cmakedefine HAVE_BZLIB @HAVE_BZLIB@
+#cmakedefine HAVE_XZLIB @HAVE_XZLIB@
 
 #endif // CONFIG_H
