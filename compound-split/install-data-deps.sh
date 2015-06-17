dfdb5e013931a3b16ba6f147d4cdc6752aa90bf9
Chris Dyer <redpony@gmail.com>
2015-06-17 13:44:24 -0400

    use downloaded data instead of github data

--- compound-split/install-data-deps.sh
+++ compound-split/install-data-deps.sh
@@ -3,7 +3,11 @@ set -e
 
 data_version=csplit-data-01.tar.gz
 
+ROOTDIR=`dirname $0`
+cd $ROOTDIR
+
 curl -f http://demo.clab.cs.cmu.edu/cdec/$data_version -o $data_version
 
 tar xzf $data_version
+rm -f $data_version
 
